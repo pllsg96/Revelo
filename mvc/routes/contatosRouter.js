38b508fa1b052d35f5a638fbc3fd22e6ca6f093a
pllsg96 <pedroluizlsg@gmail.com>
2023-09-24 22:55:22 -0300

    chore: organizando pastas

--- mvc/routes/contatosRouter.js
+++
@@ -1,9 +0,0 @@
-// routes/contatos.js
-
-const express = require('express');
-const contatosController = require('../controllers/contatosController');
-const contatosRouter = express.Router();
-
-contatosRouter.get('/', contatosController.getAllContatos);
-
-module.exports = contatosRouter;
